4a9df7949c891febc7b00259c198fed9ee3b9b18
Sarim Shaikh <sarimshaikh097@gmail.com>
2020-11-14 17:18:10 +0500

    Sales management module

---
+++ Sales-Managment-Service/src/main/java/com/sales/sys/SalesManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sales.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagmentServiceApplication.class, args);
+	}
+
+}
